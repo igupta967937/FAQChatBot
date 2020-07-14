a8f072ad5f651a6ed125894f7400405108923511
igupta967937 <61583813+igupta967937@users.noreply.github.com>
2020-07-14 16:40:52 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask_ngrok import run_with_ngrok
+from flask import Flask
+from flask import render_template,jsonify,request
+from faqengine import *
+import random
+
+
+app = Flask(__name__)
+run_with_ngrok(app)   #starts ngrok when the app 
+app.secret_key = '12345'
+
+faqslist = ["data/Greetings.csv", "data/BankFAQs.csv"]
+faqmodel = FaqEngine(faqslist)
+
+def get_response(user_message):
+    return faqmodel.query(user_message)
+
+@app.route('/')
+def hello_world():
+    return render_template('home.html')
+
+@app.route('/chat',methods=["POST"])
+def chat():
+    try:
+        user_message = request.form["text"]
+        response_text = get_response(user_message)
+        return jsonify({"status":"success","response":response_text})
+    except Exception as e:
+        print(e)
+        return jsonify({"status":"success","response":"Sorry I am not trained to do that yet..."})
+
+if __name__ == "__main__":
+    app.run()
